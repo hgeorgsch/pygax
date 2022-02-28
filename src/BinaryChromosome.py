2779b4d0d82dddf9a1e4ab4d67a3949188201c99
Hans Georg Schaathun <georg@schaathun.net>
2022-02-28 14:27:45 +0100

    Drafted GA

--- src/BinaryChromosome.py
+++ src/BinaryChromosome.py
@@ -11,68 +11,106 @@ def makeInt(x,bl):
     "Convert a binary sequence to an integer, reading the least significant bit first."
     return sum( [ x[i]*2**i for i in range(bl) ] )
 
-class BinaryChromosome:
-    def __str__(self):
-        "Make a compact display string of the the binary vector."
-        return"".join([str(x) for x in self.gene])
-    def getFloat(self):
+def BinaryRepresentation:
+    def makePopulation(self,size):
         """
-        Return the floating point vector represented by the chromosome.
-        This is the representation which should be used in the cost function
-        for a floating point problem.
+        Make a population of `size` chromosomes of dimension `dim`.
         """
-        ig = self.getIgene()
-        ignorm = ig/2**self.bits
-        p = ignorm*(self.pmax-self.pmin)+self.pmin
-        return p
-    def getIgene(self):
+        return np.random.randint(2, size( size, self.length ) )
+    def __init__(self,pmin=None,pmax=None,bits=None):
         """
-        Return an integer vector representation of the binary chromosome.
-        This is mainly an auxiliary for `getFloat()`.
-        """
-        # if self.igene != None: return self.igene
-        ss = []
-        s = 0
-        for bl in self.bits:
-            ss.append( (self.gene[s:s+bl],bl) )
-            s += bl
-        r = np.array( [ makeInt(x,bl) for (x,bl) in ss ] )
-        print("getFloat returning:", r)
-        self.igene = r
-        return r
-    def __init__(self,p=None,pmin=None,pmax=None,bits=None):
-        """
-        Creates a chromosome from the variable (vector) p.
+        Creates a mapping between floating point vectors and
+        binary chromosomes, to optimise continuous functions with
+        a binary GA.
 
         Arguments:
-            p : The variable as a scalar or numpy array
             pmin : The minimum value of each variable in p
             pmax : The maximum value of each variable in p
             bits : The number of bits to encode each variable of p
 
         **Warning** No error checking is implemented.
         """
-        gene = []
-
-        # Mormalise p to [0,1] range
-        pnorm = (p - pmin)/(pmax - pmin)
-        # Quantise pnorm and represent as an integer of required length 
-        pi = pnorm*2**bits
-        pi = np.rint(pi).astype(int)
+        self.pmax = pmax
+        self.pmin = pmin
         # If bits is a scalar, turn it into an array
         if np.isscalar(bits):
             bits = bits*np.ones(pi.size,dtype=np.int8)
+        self.bits = bits
+        self.length = sum(bits)
+    def getFloat(self,gene):
+        """
+        Return the floating point vector represented by the chromosome.
+        This is the representation which should be used in the cost function
+        for a floating point problem.
+        """
+        ig = self.getIgene(gene)
+        ignorm = ig/2**self.bits
+        p = ignorm*(self.pmax-self.pmin)+self.pmin
+        return p
+    def getIgene(self,gene):
+        """
+        Return an integer vector representation of the binary chromosome.
+        This is mainly an auxiliary for `getFloat()`.
+        """
+        ss = []
+        s = 0
+        for bl in self.bits:
+            ss.append( (gene[s:s+bl],bl) )
+            s += bl
+        r = np.array( [ makeInt(x,bl) for (x,bl) in ss ] )
+        print("getFloat returning:", r)
+        return r
+    def getGene(self,p):
+        """
+        Get the binary chromosome representting the floating point
+        vector p.
+        """
+        gene = []
+
+        # Mormalise p to [0,1] range
+        pnorm = (p - self.pmin)/(self.pmax - self.pmin)
+        # Quantise pnorm and represent as an integer of required length 
+        pi = pnorm*2**self.bits
+        pi = np.rint(pi).astype(int)
         # Binary code pi
-        for (x,b) in zip(pi,bits):
+        for (x,b) in zip(pi,self.bits):
             for i in range(b):
                 # Note! least significant bit is first
                 gene.append(x%2)
                 x >>= 1
-        self.pmax = pmax
-        self.pmin = pmin
-        self.bits = bits
-        self.gene = gene
-        self.igene = pi
+        return gene
+class BinaryChromosome:
+    def __str__(self):
+        "Make a compact display string of the the binary vector."
+        return"".join([str(x) for x in self.gene])
+    def makePopulation(size,dim):
+        """
+        Make a population of `size` chromosomes of dimension `dim`.
+        """
+        pop = np.random.randint(2, size( size, dim ) )
+        return [ BinaryChromosome(gene) for gene in pop ]
+    def __init__(self,p=None,rep=None):
+        """
+        Creates a chromosome from the variable (vector) p.
+        A representation object (e.g. `BinaryRepresentation`) is 
+        used to map between the optimisation domain and the 
+        chromosome space.
+
+        Arguments:
+            p : The variable as a scalar or numpy array
+            rep : a representation object
+
+        If p is omitted, a random chromosome is generated.
+        IF rep is omitted, p is the chromosome without conversion.
+
+        **Warning** No error checking is implemented.
+        """
+        if p == None:
+            self.gene = np.random.randint(2,size=rep.length)
+        elif rep == None:
+            self.gene = p
+        else:
+            self.gene = rep.getGene(p)
 
 # Test Data
 Amin = np.array([0,0,0])
@@ -82,7 +120,7 @@ A = np.array([1,9,4])
 if __name__ == "__main__":
     b = BinaryChromosome(A,Amin,Amax,4)
     print("Original vector:", A)
-    print("Integer representation:", b.igene)
+    # print("Integer representation:", b.igene)
     print("Chromosome representation:", b)
     print("Reconstructed after quantisation:", b.getFloat())
 
--- src/GA.py
+++ src/GA.py
@@ -2,6 +2,75 @@
 A basic, generic, GA implementation for testing and instruction.
 """
 
+from BinaryChromosome import *
+
 class GA:
-    def __init__(self):
-        pass
+    def __init__(self,representation,costfunction,mate,crossover,mutate,selectionrate=0.5):
+        """
+        Initialise the GA with a given representation and cost function.
+
+        Arguments:
+            representation : representation object defining the mapping from variables to chromosomes
+            costfunction : the costfunction to minimise
+            mate : function to select mating pairs
+            crossover : the crossover function
+            mutate : the mutation function (must work in place)
+            selectionrate : the fraction of the population to keep
+
+        Note that if the selection rate is 1 or greater, the population
+        is not sorted before mating.
+        """
+        self.costfunction = costfunction
+        self.representation = representation
+        self.mate = mate
+        self.crossover = crossover
+        self.mutate = mutate
+        self.selectionrate = selectionrate
+        self.population = None
+
+
+    def initPopulation(self,size):
+        "Initialise a population of the given size."
+        self.population = self.representation.makePopulation(size)
+        self.generation = 0
+        self.populationsize = size
+        self.Nselect = 2*ceil(self.selectionrate*size/2)
+
+    def cost(self,x):
+        return self.costfunction(x)
+
+    def evolve(self,ngen=1):
+        """Evolve the given number of generations."""
+        for i in range(ngen):
+            self.nextGeneration()
+
+    def nextGeneration(self):
+        """Evolve one generation."""
+        # Calculate the cost function; list of cost/chromosome pairs
+        cost = [ (self.cost(x),x) for x in self.population ]
+
+        # Sort and keep the best
+        if self.Nselect >= self.size:
+            cost.sort( key=lambda x : x[0] )
+            cost = cost[:self.Nselect]
+        # Note that we do not sort if we keep all the chromosomes
+
+        # Make mating pairs
+        pairs = self.mate(self.cost)
+
+        # Make offspring with the crossover function
+        offspring = [ self.crossover(x,y) for ((cx,x),(cy,y)) in pairs ]
+
+        # Add the offspring to the population
+        for x in offspring:
+            self.population.extend(x)
+
+        # Mutate
+        for x in self.population:
+            self.mutate(x)
+
+        # Advance the generation count
+        self.generation += 1
+
+        return self.population
+
