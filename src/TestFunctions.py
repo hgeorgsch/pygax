3d6e72f6fe18e1fdfd67443125b23406c9a8215d
Hans Georg Schaathun <georg@schaathun.net>
2022-03-01 14:00:06 +0100

    A string learning cost function

--- src/TestFunctions.py
+++ src/TestFunctions.py
@@ -15,3 +15,16 @@ def f5(x,n):
     l = [ np.abs(x[i]) - 10*np.cos(np.sqrt(np.abs(10*x[i]))) for i in range(n) ]
     return sum( l )
 f5d3 = lambda x : f5(x,3)
+
+def comparestring(a,b):
+    na = len(a)
+    nb = len(b)
+    d = abs(na-nb)
+    l = [ x == y for (x,y) in zip(a,b) ]
+    c = l.count(False)
+    return d + c
+
+s1 = "Like a drunk in a midnight choir"
+
+stringlearn1length = len(s1)
+def stringlearn1(x): return comparestring(x,s1)
